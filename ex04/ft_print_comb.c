14d6fb365d0f698621685ec6ac87e4fa36c31251
Melusi Mathebula <mematheb@c5r9s7.wethinkcode.co.za>
2020-07-15 10:34:32 +0200

    comb

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mematheb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 10:23:41 by mematheb          #+#    #+#             */
+/*   Updated: 2020/07/15 10:24:44 by mematheb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_comb(void)
+{
+	char	first;
+	char	second;
+	char	third;
+
+	first = '0' - 1;
+	while (++first <= '9')
+	{
+		second = first;
+		while (++second <= '9')
+		{
+			third = second + 1;
+			while (third <= '9')
+			{
+				ft_putchar(first);
+				ft_putchar(second);
+				ft_putchar(third);
+				if (first != '7' || second != '8' || third != '9')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				third++;
+			}
+		}
+	}
+}
