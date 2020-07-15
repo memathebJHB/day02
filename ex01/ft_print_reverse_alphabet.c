d16ab914c80ed05d0f9a419546cdf75add228a2b
Melusi Mathebula <mematheb@c5r9s7.wethinkcode.co.za>
2020-07-15 10:31:26 +0200

    reverse_alphabet

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void   ft_putchar(char c);
+
+void   ft_print_reverse_alphabet(void) {
+	char   start;
+	char   end;
+
+	start = 'z';
+	end = 'a';
+		while (start >= end) {
+		ft_putchar(start);
+		start--;
+	}
+}
+
+void   ft_putchar(char c) {
+	write(1, &c, 1);
+}
+int   main(void) {
+	ft_print_reverse_alphabet();
+		return (0);
+}
